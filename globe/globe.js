7446f79ca952b1d0f8651b5f217b84f787e49d32
Sundar Raman <cybertoast@gmail.com>
2012-04-21 18:03:11 -0400

    local json files and better earth shading

--- globe/globe.js
+++ globe/globe.js
@@ -49,7 +49,7 @@ DAT.Globe = function(container, colorFn) {
         'varying vec2 vUv;',
         'void main() {',
           'vec3 diffuse = texture2D( texture, vUv ).xyz;',
-          'float intensity = 1.05 - dot( vNormal, vec3( 0.0, 0.0, 1.0 ) );',
+          'float intensity = 0.5 - dot( vNormal, vec3( 0.0, 0.0, 0.0 ) );',
           'vec3 atmosphere = vec3( 1.0, 1.0, 1.0 ) * pow( intensity, 3.0 );',
           'gl_FragColor = vec4( diffuse + atmosphere, 1.0 );',
         '}'
@@ -67,7 +67,7 @@ DAT.Globe = function(container, colorFn) {
       fragmentShader: [
         'varying vec3 vNormal;',
         'void main() {',
-          'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
+          'float intensity = pow( 0.1 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
           'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
         '}'
       ].join('\n')
